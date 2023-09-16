f760eba8373c0cdf7b3f110d38c98744e8f070d6
marwaajouz <135465693+marwaajouz@users.noreply.github.com>
2023-09-16 21:52:59 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import folium
+import json
+import altair as alt
+import squarify
+import matplotlib.cm as cm
