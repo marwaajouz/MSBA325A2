321f5a5fa6ba1c48117535510375888e4b7dcbc3
marwaajouz <135465693+marwaajouz@users.noreply.github.com>
2023-09-24 18:19:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,24 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 st.title('Number of Cigarettes Consumed / Smoking Person / Day / Country')
+# Create a Streamlit slider to select the year
+selected_year = st.slider('Select Year', 2000, 2012)
+
+# Filter data for the selected year
+filtered_data = data[data['Year'] == selected_year]
+
+# Create a bar chart using Matplotlib
+fig, ax = plt.subplots()
+ax.bar(filtered_data["Country"], filtered_data["Data.Daily cigarettes"])
+ax.set_xlabel("Country")
+ax.set_ylabel("Cigarettes per Day")
+ax.set_title(f"Number of Cigarettes Consumed / Smoking Person / Day / Country ({selected_year})")
+
+# Display the chart in Streamlit
+st.pyplot(fig)
+
+
+
+
 
 
